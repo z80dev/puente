ea99c5ae7e5160c4a60072cb62cf6ba876a826b9
z80 <z80@ophy.xyz>
2023-06-21 23:46:32 -0400

    implement OmniSetter to test payload en/decoding

--- tests/test_lzapp.py
+++ tests/test_lzapp.py
@@ -15,22 +15,35 @@ def test_omnicounter(lz_mock, project, accounts):
     assert lzapp.lzEndpoint() == lz_mock.address
     lz_mock.setDestLzEndpoint(lzapp.address, lz_mock.address, sender=accounts[0])
     lz_mock.setDestLzEndpoint(lzapp2.address, lz_mock.address, sender=accounts[0])
-    print("endpoint set")
     path = encode_packed(['address', 'address'], [lzapp2.address, lzapp.address])
     path2 = encode_packed(['address', 'address'], [lzapp.address, lzapp2.address])
-    print(f"path: {path.hex()}")
     lzapp.setTrustedRemote(101, path, sender=accounts[0])
     lzapp2.setTrustedRemote(101, path2, sender=accounts[0])
     # lzapp.setTrustedRemoteAddress(101, lzapp.address, sender=accounts[0])
-    print("trusted remote set")
     # embed to debug
     # IPython.embed()
     # print trusted remote address in hex
-    print(f"trusted remote address: {lzapp.getTrustedRemoteAddress(101).hex()}")
     assert lzapp.getTrustedRemoteAddress(101).hex() == '0x' + bytes.fromhex(lzapp2.address[2:]).hex()
-    print(lzapp.trustedRemoteLookup(101).hex())
 
     r = lzapp.incrementCounter(101, sender=accounts[0], value=1000000000000000000)
-    for e in r.events:
-        print(e)
     assert lzapp2.counter() == 1
+
+
+def test_omnisetter(lz_mock, project, accounts):
+    lzapp = project.OmniSetter.deploy(lz_mock, sender=accounts[0])
+    lzapp2 = project.OmniSetter.deploy(lz_mock, sender=accounts[0])
+    assert lzapp.lzEndpoint() == lz_mock.address
+    lz_mock.setDestLzEndpoint(lzapp.address, lz_mock.address, sender=accounts[0])
+    lz_mock.setDestLzEndpoint(lzapp2.address, lz_mock.address, sender=accounts[0])
+    path = encode_packed(['address', 'address'], [lzapp2.address, lzapp.address])
+    path2 = encode_packed(['address', 'address'], [lzapp.address, lzapp2.address])
+    lzapp.setTrustedRemote(101, path, sender=accounts[0])
+    lzapp2.setTrustedRemote(101, path2, sender=accounts[0])
+    # lzapp.setTrustedRemoteAddress(101, lzapp.address, sender=accounts[0])
+    # embed to debug
+    # IPython.embed()
+    # print trusted remote address in hex
+    assert lzapp.getTrustedRemoteAddress(101).hex() == '0x' + bytes.fromhex(lzapp2.address[2:]).hex()
+
+    r = lzapp.incrementCounter(101, 5, sender=accounts[0], value=1000000000000000000)
+    assert lzapp2.counter() == 5
